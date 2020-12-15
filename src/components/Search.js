98b22953ee6df928c9866f4cbb66fb982e4f2395
Ramtin <31255934+rradfar@users.noreply.github.com>
2020-12-15 17:41:43 +0400

    Add axios

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('');
 
-  useEffect(() => {}, [term]);
+  useEffect(() => {
+    const search = async () => {
+      await axios.get();
+    };
+
+    search();
+  }, [term]);
 
   return (
     <div>

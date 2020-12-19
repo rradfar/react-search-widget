1db345bb665893eeb5960054250d0e665ae42313
Ramtin <31255934+rradfar@users.noreply.github.com>
2020-12-19 15:02:17 +0400

    Throttle API requests

--- src/components/Search.js
+++ src/components/Search.js
@@ -19,8 +19,18 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    if (term) {
+    if (term && !results.length) {
       search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 1000);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
     }
   }, [term]);
 

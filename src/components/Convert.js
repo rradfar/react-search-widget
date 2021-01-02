741abfa4d90b9b270e3aa9dee30debed0b4966b4
Ramtin <31255934+rradfar@users.noreply.github.com>
2021-01-02 09:39:39 -0800

    Add the Convert component

---
+++ src/components/Convert.js
@@ -0,0 +1,9 @@
+// AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms - IwDlM;
+import React, { useState, useEffect } from 'react';
+
+const Convert = ({ language, text }) => {
+  useEffect(() => {}, [language, text]);
+  return <div />;
+};
+
+export default Convert;
